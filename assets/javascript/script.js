b35cc320da99dd9d32f78d0a64887bc9f2593366
Nuruddin-Adnan <contact.codepixel@gmail.com>
2022-12-30 21:10:05 +0600

    clock and banner slider added

---
+++ assets/javascript/script.js
@@ -0,0 +1,39 @@
+// world clock slider
+const clockSwiper = new Swiper(".clockSwiper", {
+    slidesPerView: 2,
+    spaceBetween: 5,
+    breakpoints: {
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 0,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 0,
+        },
+        1200: {
+            slidesPerView: 8,
+            spaceBetween: 0,
+        },
+        1396: {
+            slidesPerView: 10,
+            spaceBetween: 0,
+        },
+    },
+    navigation: {
+        nextEl: ".world-clock-next",
+        prevEl: ".world-clock-prev",
+    },
+});
+
+
+// Banner slider
+const bannerSwiper = new Swiper(".bannerSwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
